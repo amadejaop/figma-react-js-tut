7885b11b196147159cb46ec615b34907aa8684ba
Amadeja <obsteter.amadeja@gmail.com>
2024-02-12 05:41:43 +0100

    Create a react app and the components
    Created a react app and added components with basic template and css
    file imports.

---
+++ front-end/src/components/info/Info.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Info.css";
+
+const Info = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Info
---
+++ front-end/src/components/topics/Topics.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Topics.css";
+
+const Topics = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Topics
