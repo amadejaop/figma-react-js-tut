beb0497a7b50acedf41e20133438623db2e424e8
Amadeja <obsteter.amadeja@gmail.com>
2024-02-13 06:30:41 +0100

    Add HTML and CSS for the blog section

--- front-end/src/components/blog/Blog.jsx
+++ front-end/src/components/blog/Blog.jsx
@@ -1,11 +1,49 @@
 import React from 'react';
 import "./Blog.css";
+import { blog1, blog2, blog3 } from "../../assets"
 
 const Blog = () => {
   return (
-    <div>
-      blog
-    </div>
+    <section id='blog' className='pink'>
+      <div className="wrapper">
+        <h2 className="light">Latest Posts</h2>
+        <div className="content-container">
+          <div className="post">
+            <div className="tag">DAW</div>
+            <a href="#">
+              <img src={blog1} alt="" />
+            </a>
+            <a href="#">
+              <h3 className="post-title">How to use drum machine in logic pro X</h3>
+            </a>
+          </div>
+
+          <div className="post">
+            <div className="tag">Mixing</div>
+            <a href="#">
+              <img src={blog2} alt="" />
+            </a>
+            <a href="#">
+              <h3 className="post-title">How to mix guitars effectively</h3>
+            </a>
+          </div>
+
+          <div className="post">
+            <div className="tag">VOX</div>
+            <a href="#">
+              <img src={blog3} alt="" />
+            </a>
+            <a href="#">
+              <h3 className="post-title">The real power of harmonies in music production</h3>
+            </a>
+          </div>
+        </div>
+
+        <div className="btn-container">
+          <a href="#" className='all-posts-btn'>All Posts</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
