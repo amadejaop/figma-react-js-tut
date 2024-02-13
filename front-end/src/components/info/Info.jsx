d2e7a3a3fe53a9c6ca29ee400936b19c2bca2f4a
Amadeja <obsteter.amadeja@gmail.com>
2024-02-13 06:13:11 +0100

    Add HTML and CSS for info section
    Added HTML to the Info.jsx file and CSS to the Info.css file to style
    the section of the page.

--- front-end/src/components/info/Info.jsx
+++ front-end/src/components/info/Info.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import "./Info.css";
+import { studentIcon, videoIcon, bgElement2 } from "../../assets"
 
 const Info = () => {
   return (
-    <div>
-      infoooo
-    </div>
+    <section id="info" className='dark-gray'>
+      <div className="wrapper">
+        <div className="content-container">
+          <div className="info-content">
+            <img src={studentIcon} alt="Student icon" 
+            />
+            <div className="amount">23,000+</div>
+            <div className="type">Students</div>
+          </div>
+
+          <div className="info-content">
+            <img src={videoIcon} alt="Video icon" 
+            />
+            <div className="amount">26 hrs</div>
+            <div className="type">Video Content</div>
+          </div>
+        </div>
+        <img src={bgElement2} alt="" className='bg-element2' />
+      </div>
+    </section>
   )
 }
 
