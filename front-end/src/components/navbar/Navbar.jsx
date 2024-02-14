8546c96058f5cb06f81685a8f9bfbfe5e439e1be
Amadeja <obsteter.amadeja@gmail.com>
2024-02-14 07:03:16 +0100

    Add mobile navbar, add proper links to the nav items
    Added links to the specific sections of the website to the navigation
    bar.
    Added a functioning mobile/hamburger menu.

--- front-end/src/components/navbar/Navbar.jsx
+++ front-end/src/components/navbar/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Navbar.css";
 import { menuIcon, closeIcon } from "../../assets"
 
 const Navbar = () => {
+  const [isActive, setIsActive] = useState(false);
+
   return (
     <>
     <nav>
@@ -22,27 +24,27 @@ const Navbar = () => {
         </li>
       </ul>
 
-      <div className="menu-icon">
+      <div onClick={() => setIsActive(true)} className="menu-icon">
         <img src={menuIcon} alt="" />
       </div>
     </nav>
 
-    <div className="mobile-menu-container">
-      <div className="close-icon">
+    <div className={`mobile-menu-container ${isActive ? "active" : ""}`}>
+      <div onClick={() => setIsActive(false)} className="close-icon">
         <img src={closeIcon} alt="" />
       </div>
       <ul className='menu-items'>
         <li>
-          <a href="#topics">Course Details</a>
+          <a href="#topics" onClick={() => setIsActive(false)}>Course Details</a>
         </li>
         <li>
-          <a href="#info">About</a>
+          <a href="#info" onClick={() => setIsActive(false)}>About</a>
         </li>
         <li>
-          <a href="#blog">Blog</a>
+          <a href="#blog" onClick={() => setIsActive(false)}>Blog</a>
         </li>
         <li>
-          <a href="#testimonials">Testimonials</a>
+          <a href="#testimonials" onClick={() => setIsActive(false)}>Testimonials</a>
         </li>
       </ul>
     </div>
